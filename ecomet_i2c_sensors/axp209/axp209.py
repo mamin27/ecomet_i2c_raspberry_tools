4e82d793c857d7d5048fd6cb667f9ed044fb84df
Marian Minar <mminar7@gmail.com>
2023-11-21 07:40:01 +0000

    Add ADC enable register

--- ecomet_i2c_sensors/axp209/axp209.py
+++ ecomet_i2c_sensors/axp209/axp209.py
@@ -10,7 +10,8 @@ reg_list = { 'POWER_OPERATING_MODE_REG' : axp209_constant.POWER_OPERATING_MODE_R
              'BATTERY_GAUGE_REG' : axp209_constant.BATTERY_GAUGE_REG,
              'INTERNAL_TEMPERATURE_REG' : axp209_constant.INTERNAL_TEMPERATURE_REG, 'BATTERY_VOLTAGE_REG' : axp209_constant.BATTERY_VOLTAGE_REG,
              'BATTERY_DISCHARGE_CURRENT_REG' : axp209_constant.BATTERY_DISCHARGE_CURRENT_REG, 'BATTERY_VOLTAGE_REG' : axp209_constant.BATTERY_VOLTAGE_REG,
-             'VBUS_IPSOUT_CHANNEL_MANAGEMENT_REG' : axp209_constant.VBUS_IPSOUT_CHANNEL_MANAGEMENT_REG
+             'VBUS_IPSOUT_CHANNEL_MANAGEMENT_REG' : axp209_constant.VBUS_IPSOUT_CHANNEL_MANAGEMENT_REG,
+             'ADC_ENABLE1_REG' : axp209_constant.ADC_ENABLE1_REG, 'ADC_ENABLE2_REG' : axp209_constant.ADC_ENABLE2_REG
         }
 
 logger = logging.getLogger(__name__)
@@ -26,9 +27,6 @@ class AXP209(object):
             i2c = I2C
         self._logger = logging.getLogger(__name__)    
         self._device = i2c.get_i2c_device(address, busnum, **kwargs)
-        
-        # force ADC enable for battery voltage and current
-        self.adc_enable1 = 0xc3
         
     def __enter__(self):
         return self
@@ -51,12 +49,12 @@ class AXP209(object):
             flags.gpio_function = 0b111
         else:
             flags.gpio_function = 0b000
-        self.bus.write_byte_data(AXP209_ADDRESS, GPIO2_FEATURE_SET_REG, flags.asbyte)
+        #self.bus.write_byte_data(AXP209_ADDRESS, GPIO2_FEATURE_SET_REG, flags.asbyte)
 
     @property
     def adc_enable1(self):
-        flags = ADC_ENABLE1_FLAGS()
-        flags.asbyte = self.bus.read_byte_data(AXP209_ADDRESS, ADC_ENABLE1_REG)
+        flags = axp209_constant.ADC_ENABLE1_FLAGS()
+        (flags.asbyte,ret) = self.read_register('ADC_ENABLE1_REG')
         return flags
 
     @adc_enable1.setter
@@ -66,7 +64,19 @@ class AXP209(object):
  #       self.bus.write_byte_data(AXP209_ADDRESS, ADC_ENABLE1_REG, flags)
 
     @property
-    def vbus_read_current_limit(self):
+    def adc_enable2(self):
+        flags = axp209_constant.ADC_ENABLE2_FLAGS()
+        (flags.asbyte,ret) = self.read_register('ADC_ENABLE2_REG')
+        return flags
+
+    @adc_enable2.setter
+    def adc_enable2(self, flags):
+        if hasattr(flags, "asbyte"):
+            flags = flags.asbyte
+ #       self.bus.write_byte_data(AXP209_ADDRESS, ADC_ENABLE2_REG, flags)
+
+    @property
+    def vbus_current_limit(self):
         """ Returns the current vbus current limit setting """
         limits = { #00:900 mA; 01:500 mA; 10:100 mA; 11: not limit
             0: "900 mA",
@@ -78,8 +88,8 @@ class AXP209(object):
         current_limit = current_data & 0x03
         return limits.get(current_limit, "invalid setting")
 
-    #@vbus_current_limit.setter
-    def vbus_write_current_limit(self, val):
+    @vbus_current_limit.setter
+    def vbus_current_limit(self, val):
         flags = axp209_constant.VBUS_CURRENT_LIMIT_CONTROL()
         limits = { #00:900 mA; 01:500 mA; 10:100 mA; 11: not limit
             0: "900 mA",
@@ -109,6 +119,48 @@ class AXP209(object):
         (flags.asbyte,ret) = self.read_register('POWER_OPERATING_MODE_REG')
         return flags
 
+    # ADC_ENABLE1 REGISTER status
+    @property
+    def battery_current_adc_enable(self):
+        return bool(self.adc_enable1.battery_current_adc_enable)
+
+    @property
+    def acin_voltage_adc_enable(self):
+        return bool(self.adc_enable1.acin_voltage_adc_enable)
+
+    @property
+    def acin_current_adc_enable(self):
+        return bool(self.adc_enable1.acin_current_adc_enable)
+
+    @property
+    def vbus_voltage_adc_enable(self):
+        return bool(self.adc_enable1.vbus_voltage_adc_enable)
+
+    @property
+    def vbus_current_adc_enable(self):
+        return bool(self.adc_enable1.vbus_current_adc_enable)
+
+    @property
+    def aps_voltage_adc_enable(self):
+        return bool(self.adc_enable1.aps_voltage_adc_enable)
+
+    @property
+    def ts_pin_adc_function_enable(self):
+        return bool(self.adc_enable1.ts_pin_adc_function_enable)
+
+    # TBD_ENABLE2_REGISTER status
+    @property
+    def apx209_internal_temperature_monitoring_adc_enable(self):
+        return bool(self.adc_enable2.apx209_internal_temperature_monitoring_adc_enable)
+
+    @property
+    def gpio0_adc_function_enable(self):
+        return bool(self.adc_enable2.gpio0_adc_function_enable)
+
+    @property
+    def gpio1_adc_function_enable(self):
+        return bool(self.adc_enable2.gpio1_adc_function_enable)
+    #    
     @property
     def battery_exists(self):
         return bool(self.power_operating_mode.battery_exists)
@@ -176,7 +228,8 @@ class AXP209(object):
               reg_status = reg_status_hex_high << 5 | reg_status_hex_low & 0x1f
            except :
               ret = ret + 1
-        elif register in ['POWER_OPERATING_MODE_REG','POWER_INPUT_STATUS_REG','BATTERY_GAUGE_REG','BATTERY_GAUGE_REG','VBUS_IPSOUT_CHANNEL_MANAGEMENT_REG'] :
+        elif register in ['POWER_OPERATING_MODE_REG','POWER_INPUT_STATUS_REG','BATTERY_GAUGE_REG','BATTERY_GAUGE_REG','VBUS_IPSOUT_CHANNEL_MANAGEMENT_REG',
+                          'ADC_ENABLE1_REG','ADC_ENABLE2_REG'] :
            try :
               reg_status = self._device.readU8(reg_list[register])
            except :
--- ecomet_i2c_sensors/axp209/axp209_constant.py
+++ ecomet_i2c_sensors/axp209/axp209_constant.py
@@ -9,6 +9,7 @@ POWER_OPERATING_MODE_REG 			= 0x01
 CHARGE_CONTROL_REG 					= 0x33
 CHARGE_CONTROL2_REG 				= 0x34
 ADC_ENABLE1_REG 					= 0x82
+ADC_ENABLE2_REG						= 0x83
 INTERNAL_TEMPERATURE_REG 			= 0x5e
 BATTERY_VOLTAGE_REG 				= 0x78
 BATTERY_CHARGE_CURRENT_REG 			= 0x7a
@@ -107,100 +108,36 @@ class VBUS_CURRENT_LIMIT_CONTROL(Union):
                 ("asbyte", c_uint8)]
 
     _anonymous_ = ("_b",)
-    
-						    
 
+class ADC_ENABLE1_FLAGS(Union):
+    class _b(BigEndianStructure):
+        _fields_ = [
+            ("battery_voltage_adc_enable", c_uint8, 1),
+            ("battery_current_adc_enable", c_uint8, 1),
+            ("acin_voltage_adc_enable", c_uint8, 1),
+            ("acin_current_adc_enable", c_uint8, 1),
+            ("vbus_voltage_adc_enable", c_uint8, 1),
+            ("vbus_current_adc_enable", c_uint8, 1),
+            ("aps_voltage_adc_enable", c_uint8, 1),
+            ("ts_pin_adc_function_enable", c_uint8, 1),
+        ]
 
+    _fields_ = [("_b", _b),
+                ("asbyte", c_uint8)]
 
-       
+    _anonymous_ = ("_b",)
 
-TIME_100MS         = 0x00   # 100 ms
-TIME_200MS         = 0x01   # 200 ms
-TIME_300MS         = 0x02   # 300 ms
-TIME_400MS         = 0x03   # 400 ms
-TIME_500MS         = 0x04   # 500 ms
-TIME_600MS         = 0x05   # 600 ms
+class ADC_ENABLE2_FLAGS(Union):
+    class _b(BigEndianStructure):
+        _fields_ = [
+            ("apx209_internal_temperature_monitoring_adc_enable", c_uint8, 1),
+            ("_reserved_", c_uint8, 3),
+            ("gpio0_adc_function_enable", c_uint8, 1),
+            ("gpio1_adc_function_enable", c_uint8, 1),
+            ("_reserved_", c_uint8, 2),
+        ]
 
+    _fields_ = [("_b", _b),
+                ("asbyte", c_uint8)]
 
-# Enumeration for the persistance filter
-PERSIST_EVERY      = 0x00   # Every ALS cycle generates an interrupt
-PERSIST_ANY		   = 0x01   # Any value otside of threshold range
-PERSIST_2		   = 0x02   # 2 consecutive values out of range
-PERSIST_3		   = 0x03   # 3 consecutive values out of range
-PERSIST_5		   = 0x04   # 5 consecutive values out of range
-PERSIST_10		   = 0x05   # 10 consecutive values out of range
-PERSIST_15		   = 0x06   # 15 consecutive values out of range
-PERSIST_20		   = 0x07   # 20 consecutive values out of range
-PERSIST_25		   = 0x08   # 25 consecutive values out of range
-PERSIST_30		   = 0x09   # 30 consecutive values out of range
-PERSIST_35		   = 0x0a   # 35 consecutive values out of range
-PERSIST_40		   = 0x0b   # 40 consecutive values out of range
-PERSIST_45		   = 0x0c   # 45 consecutive values out of range
-PERSIST_50		   = 0x0d   # 50 consecutive values out of range
-PERSIST_55		   = 0x0e   # 55 consecutive values out of range
-PERSIST_60		   = 0x0f   # 60 consecutive values out of range
-
-# Enumeration for the sensor gain
-GAIN_LOW           = 0x00  # low gain (1x)
-GAIN_MED           = 0x10  # medium gain (25x)
-GAIN_HIGH          = 0x20  # medium gain high (428x)
-GAIN_MAX           = 0x30  # max gain (9876x)
-
-# LUX coefficient
-#LUX_DF             = '408.0F' # Lux coefficient
-#LUX_COEFB		   = '1.64F'  # CH0 coefficient
-#LUX_COEFC		   = '0.59F'  # CH1 coefficient A
-#LUX_COEFD		   = '0.86F'  # CH2 coefficient B
-
-# Commands
-COMMAND_BIT		   = 0xa0   # 1010 0000: bits 7 and 5 for 'command normal'
-#CLEAR_INT		   = 0xe7   # Special Function Command for "Clear ALS and no persist ALS interrupt"
-#TEST_INT		   = 0xe4   # Special Function Command for "Interrupt set - forces an interrupt"
-#WORD_BIT		   = 0x20   # < 1 = read/write word (rather than byte)
-#LOCK_BIT		   = 0x10   # < 1 = using block read/write
-
-# Enable Mask Bit
-ENABLE_NPIEN	   = 0x80   # No Persist Interrupt Enable. When asserted NP Threshold conditions will generate an interrupt, bypassing the persist filter.
-ENABLE_SAI		   = 0x20   # Sleep after interrupt. When asserted, the device will power down at the end of an ALS cycle if an interrupt has been generated.
-ENABLE_AIEN 	   = 0x10   # ALS Interrupt Enable. When asserted permits ALS interrupts to be generated, subject to the persist filter.
-ENABLE_AEN		   = 0x02   # ALS Enable. This field activates ALS function. Writing a one activates the ALS. Writing a zero disables the ALS.
-ENABLE_POWER	   = 0x01   # Flag for ENABLE register to enable intrnal oscillator
-DISABLE_NPIEN	   = 0x3f
-DISABLE_SAI		   = 0x5f
-DISABLE_AIEN	   = 0xef
-DISABLE_AEN		   = 0xfd
-DISABLE_POWER	   = 0x00   # Flag for ENABLE register to disable internal oscillator
-
-# Control Mask Bit
-CTLR_RESET		   = 0x80   # System reset. When asserted, the device will reset equivalent to a power-on reset. SRESET is self-clearing.
-CTLR_AGAIN		   = 0x30   # ALS gain sets the gain of the internal integration amplifiers for both photodiode channels.
-CTLR_ATIME		   = 0x07   # ALS time sets the internal ADC integration time for both photodiode channels.
-
-# Mask Bits
-PERSIST			   = 0x0f   # ALS interrupt persistence filter
-PID_MASK		   = 0x18   # DEVICE_PID MASK
-NPINTR_MASK		   = 0x10   # No-persist Interrupt. Indicates that the device has encountered a no-persist interrupt condition. MASK
-AINT_MASK		   = 0x08   # ALS Interrupt. Indicates that the device is asserting an ALS interrupt. MASK
-AVALID_MASK		   = 0x01   # ALS Valid. Indicates that the ADC channels have completed an integration cycle since the AEN bit was asserted.
-
-#LUX_DB coeficient
-LUX_DF			   = 408.0
-LUX_COEFB		   = 1.64
-LUX_COEFC		   = 0.59
-LUX_COEFD		   = 0.86
-MAX_COUNT_100MS	   = (36863)
-MAX_COUNT		   = (65535)
-
-#Special Commands
-SetInterrupt	     = 0xe4 # Interrupt set  forces an interrupt
-ClearAlsInt		     = 0xe6 # Clears ALS interrupt
-ClearAlsNoPersAlsInt = 0xe7 # Clears ALS and no persist ALS interrupt
-ClearsNoPersAlsInt   = 0xea # Clears no persist ALS interrupt
-
-# The interrupt set special function command sets the interrupt bits 
-# in the status register (0x13). For the interrupt to be visible on the 
-# INT pin, one of the interrupt enable bits in the enable register 
-# (0x00) must be asserted.
-# The interrupt set special function must be cleared with an interrupt 
-# clear special function. The ALS interrupt clear special functions 
-# clear any pending interrupt(s) and are self-clearing.
+    _anonymous_ = ("_b",)
